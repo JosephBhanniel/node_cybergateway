4eaa09a458c14f4936ae67d73af65f0920a79feb
Joseph Banda <124339691+JosephBhanniel@users.noreply.github.com>
2024-06-10 09:39:21 +0200

    Update Configuration.js

--- Data/Configuration.js
+++ Data/Configuration.js
@@ -13,14 +13,6 @@ const MerchantId = 'testrest';
 const MerchantKeyId = '08c94330-f618-42a3-b09d-e1e43be5efda';
 const MerchantSecretKey = 'yBJxy6LjM2TmcPGu+GaJrHtkke25fPpUX+UY6/L/1tE=';
 
-// // common parameters
-// const AuthenticationType = 'http_signature';
-// const RunEnvironment = 'apitest.cybersource.com';
-// const MerchantId = 'joseph05_1717580099';
-
-// // http_signature parameters
-// const MerchantKeyId = '156f2312-0132-42ec-91da-5681bb35450c';
-// const MerchantSecretKey = 'iqsVXVFRSvv/eDm5MvRrtMgPr2sDnUvrnit0ijhpAVg=';
 
 
 // jwt parameters
